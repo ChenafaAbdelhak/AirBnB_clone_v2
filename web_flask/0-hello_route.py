0bc2ac2e6917f51253f932903d38a1379b48f59a
ChenafaAbdelhak <chenafa.abdelhak@gmail.com>
2024-07-30 11:05:11 +0000

    first route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""starts flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+	"""
+	function that returns Hello HBNB!.
+	"""
+	return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+	app.url_map.sctict_stashes = False
+	app.run(host="0.0.0.0", port=5000)
